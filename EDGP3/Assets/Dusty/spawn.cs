a244845ca72d4bf9b7ada921ce7e49c86bef53e0
mocchii <d.dan342@gmail.com>
2014-10-14 20:38:19 -0400

    Updated patterns

--- EDGP3/Assets/Dusty/spawn.cs
+++ EDGP3/Assets/Dusty/spawn.cs
@@ -10,9 +10,11 @@ public class spawn : MonoBehaviour {
 	int y = 10;
 	public float enter;
 	float time;
+	int i = 0;
 	public bool summon = false;
 	// Use this for initialization
 	void Start () {
+
 		time = Time.time;
 
 		
@@ -22,9 +24,9 @@ public class spawn : MonoBehaviour {
 	void Update () {
 
 		if(time + enter <= Time.time && summon){
-			Diagonal(1,1,3);
-			Down (1,0,3);
+			WaveM(i);
 			time = Time.time;
+			i++;
 		}
 	}
 	public void starter(){
@@ -40,6 +42,20 @@ public class spawn : MonoBehaviour {
 			test.GetComponent<Enemy>().attacktype = type;
 		}
 	}
+	public void Side(int numene,int start,int type,int dir){
+		numene += 7;
+		int a;
+		if(dir == 0){
+			a = 1;
+		}else
+			a = -1;
+		for(int i = numene; i > 7; i--){
+			GameObject test;
+			test = Instantiate(enemy, new Vector3(i*a, start, 0), Quaternion.identity) as GameObject;
+			test.GetComponent<Enemy>().changeloc(new Vector3(-i*a, start, 0));
+			test.GetComponent<Enemy>().attacktype = type;
+		}
+	}
 	public void Diagonal(int numene,int dir,int type){
 		numene += 6;
 		for(int i = numene; i > 6; i--){
@@ -50,6 +66,13 @@ public class spawn : MonoBehaviour {
 				test = Instantiate(enemy, new Vector3(-i, i, 0), Quaternion.identity) as GameObject;
 			test.GetComponent<Enemy>().changeloc(new Vector3(i, -i, 0));
 			test.GetComponent<Enemy>().attacktype = type;
+		}
+	}
+	void WaveM(int i){
+		if(i == 0){
+			Diagonal(2,1,3);
+			Down (1,0,3);
+			Side (1,0,3,1);
 		}
 	}
 	
