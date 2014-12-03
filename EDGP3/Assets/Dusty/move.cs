1d6b2dbd3dae8852a0ba5135642325d20843d025
mocchii <d.dan342@gmail.com>
2014-12-03 04:48:17 -0500

    Fixed parallax?

--- EDGP3/Assets/Dusty/move.cs
+++ EDGP3/Assets/Dusty/move.cs
@@ -2,14 +2,21 @@
 using System.Collections;
 
 public class move : MonoBehaviour {
-	public GameObject master;
+	GameObject master;
+	GameObject mang;
 	// Use this for initialization
 	void Start () {
 		master = GameObject.FindGameObjectWithTag("fog");
+		mang = GameObject.FindGameObjectWithTag("Manager");
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if(mang.GetComponent<spawn>().stage == 1){
+			renderer.enabled = true;
+		}else{
+			renderer.enabled = false;
+		}
 		GetComponent<SpriteRenderer>().sprite = master.GetComponent<SpriteRenderer>().sprite;
 	}
 }
