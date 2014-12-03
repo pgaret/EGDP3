90bce7598e7a16be06be325c5468fd461e32bb75
Peregrin Garet <peregringaret@gmail.com>
2014-12-03 00:50:23 -0500

    MMShield is working.  Mirror sprites on relevant objects only show up if MM is present & in attack mode.

--- EDGP3/Assets/Peregrin's Work/Scripts/MMShield.cs
+++ EDGP3/Assets/Peregrin's Work/Scripts/MMShield.cs
@@ -12,12 +12,42 @@ public class MMShield : MonoBehaviour {
 		if (option1.GetComponent<PlayerStats>().role == "Defender") transform.parent = option1.transform;
 		else transform.parent = GameObject.FindGameObjectWithTag("Player2").transform;
 		
-//		Debug.Log (transform.parent.name);
+// 		Debug.Log (transform.parent.name);
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-	
+		char affinity = transform.parent.GetComponent<PlayerStats>().affinity;
+		if (affinity == 'A') GetComponent<Animator>().SetBool("typea", true);
+		else GetComponent<Animator>().SetBool("typea", false);
+		
+		if (affinity == 'A')
+		{
+			GameObject[] bullets = GameObject.FindGameObjectsWithTag("BulletA");
+			foreach (GameObject bullet in bullets)
+			{
+				if (bullet.collider.bounds.Intersects(renderer.bounds))
+				{
+					transform.parent.GetComponent<PlayerStats>().ammo += 1;
+					Destroy (bullet);
+				}
+				
+			}
+		}
+		else
+		{
+			GameObject[] bullets = GameObject.FindGameObjectsWithTag("BulletB");
+			foreach (GameObject bullet in bullets)
+			{
+				if (bullet.collider.bounds.Intersects(renderer.bounds))
+				{
+					transform.parent.GetComponent<PlayerStats>().ammo += 1;
+					Destroy(bullet);
+				}
+				
+			}
+		}
+
 	}
 }
