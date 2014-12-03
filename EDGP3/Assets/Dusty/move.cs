68a8bd58fc788339f4eaf74870a415d6d7ef5b17
mocchii <d.dan342@gmail.com>
2014-12-03 04:42:54 -0500

    parallax?

---
+++ EDGP3/Assets/Dusty/move.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+	public GameObject master;
+	// Use this for initialization
+	void Start () {
+		master = GameObject.FindGameObjectWithTag("fog");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<SpriteRenderer>().sprite = master.GetComponent<SpriteRenderer>().sprite;
+	}
+}
