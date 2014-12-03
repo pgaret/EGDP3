697b12294dd92f2c6d460a98bdc9f9d63b819272
PremiumGas <regaf3@gmail.com>
2014-12-03 03:46:28 -0500

    Player death and level up sprites. MM slowed down.

--- EDGP3/Assets/Peregrin's Work/Scripts/DeadEnemy.cs
+++ EDGP3/Assets/Peregrin's Work/Scripts/DeadEnemy.cs
@@ -25,22 +25,23 @@ public class DeadEnemy : MonoBehaviour {
 	{
 		if (type != -1 && hasAnim == false)
 		{
-			//Blue
-			if (type == 5 || type == 13 || type == 22) anim.SetInteger("type", 1);
-			//Brown
-			if (type == 3 || type == 12 || type == 20) anim.SetInteger("type", 2);
-			//Green
-			if (type == 4 || type == 11 || type == 21) anim.SetInteger("type",3);
-			//Grey
-			if (type == 7 || type == 14 || type == 23) anim.SetInteger("type", 4);
-			//Metallic
-			if (type == 16 || type == 25 || type == 25) anim.SetInteger("type", 5);
-			//Pink
-			if (type == 8 || type == 17 || type == 27) anim.SetInteger("type", 6);
-			//Purple
-			if (type == 6 || type == 15 || type == 24) anim.SetInteger("type", 7);
 			//Yellow
-			if (type == 2 || type == 10 || type == 19) anim.SetInteger("type", 8);
+			if (type == 01 || type == 09 || type == 18) anim.SetInteger("type", 8);
+			//Brown
+			if (type == 02 || type == 11 || type == 19) anim.SetInteger("type", 2);
+			//Green
+			if (type == 03 || type == 10 || type == 20) anim.SetInteger("type", 3);
+			//Blue
+			if (type == 04 || type == 12 || type == 21) anim.SetInteger("type", 1);
+			//Purple
+			if (type == 05 || type == 14 || type == 23) anim.SetInteger("type", 7);
+			//Dark Grey
+			if (type == 06 || type == 15 || type == 24) anim.SetInteger("type", 4);
+			//Pink
+			if (type == 07 || type == 16 || type == 25) anim.SetInteger("type", 6);
+			//Metallic
+			if (type == 13 || type == 22 || type == 22) anim.SetInteger("type", 5);
+						
 			hasAnim = true;
 		}
 		if (Time.time > timer) Destroy (gameObject);
