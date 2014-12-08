f306834cddc9aa32e92911451fcf09c52015d0c1
Peregrin Garet <peregringaret@gmail.com>
2014-12-07 20:48:29 -0500

    Score at the end (kinda high score...sorta...definitely score though)

---
+++ EDGP3/Assets/Peregrin's Work/Scripts/Score.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+	public float score;
+
+	// Use this for initialization
+	void Start ()
+	{
+		score = 0;
+		DontDestroyOnLoad (gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Application.loadedLevelName == "Alex's Scene")
+		{
+			Application.LoadLevel("Peregrin's Scene");
+		}
+		if (Application.loadedLevelName == "Peregrin's Scene")
+		{
+			if (GameObject.Find ("Main Camera").GetComponent<GUIManager>().mode == 2)
+			{
+				float potentialScore = GameObject.FindGameObjectWithTag("Player1").GetComponent<PlayerStats>().score + GameObject.FindGameObjectWithTag("Player2").GetComponent<PlayerStats>().score;
+				if (potentialScore > score) score = potentialScore;
+			}
+		}
+	}
+}
